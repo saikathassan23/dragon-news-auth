07a6e0af4faceca099f20825047f8ba0ee58664b
saikat <saikathassan23@gmail.com>
2023-10-03 21:48:37 +0600

    env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,14 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAvou7bwZkXmPpDKxYRS3Jpf-2zXbaohB0',
-  authDomain: 'dragon-news-auth-7c493.firebaseapp.com',
-  projectId: 'dragon-news-auth-7c493',
-  storageBucket: 'dragon-news-auth-7c493.appspot.com',
-  messagingSenderId: '978498656958',
-  appId: '1:978498656958:web:642d0de85cb05d589d9cef',
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
